f78886c5e2d5ee1791ec105049be4a8ab749ae82
uvok <gh@uvokchee.de>
2021-11-20 21:15:10 +0100

    Beautify input

--- setlimit.py
+++ setlimit.py
@@ -3,6 +3,11 @@ from pprint import pprint as pp
 import time
 import ccxt
 from ccxt.base import errors as err
+try:
+    import questionary
+    vprompt=1
+except ImportError:
+    vprompt=0
 import config
 
 kraken = ccxt.kraken({
@@ -12,8 +17,30 @@ kraken = ccxt.kraken({
 })
 
 kraken.checkRequiredCredentials()
+def my_input(query):
+    if vprompt:
+        return questionary.text(query).ask()
+    else:
+        return input(query+" ")
 
-pair = input("Enter currency pair: ")
+def my_select(query, options):
+    if vprompt:
+        return questionary.select(
+            query,
+            choices=options
+        ).ask()
+    else:
+        txt = f"{query} ({', '.join(options)}) "
+        return input(txt)
+
+def my_confirm(query):
+    if vprompt:
+        return questionary.confirm(query, default=False, auto_enter=False).ask()
+    else:
+        reply = my_input(query)
+        reply = reply.lower()[0]
+        return reply[0] == "y"
+
 
 def get_market_price(exchange, pair):
     # limit to 1
@@ -23,15 +50,24 @@ def get_market_price(exchange, pair):
     spread = (ask - bid) if (bid and ask) else None
     return [bid, ask, spread]
 
-print("Current market: ")
-prices = get_market_price(kraken, pair)
-print(prices)
+
+valid_sym=False
+while not valid_sym:
+    try:
+        pair = my_input("Enter currency pair:")
+        print("Current market: ")
+        prices = get_market_price(kraken, pair)
+        print(prices)
+        valid_sym = True
+    except err.BadSymbol:
+        print("Invalid pair")
 
 curr = ""
 while not curr in ["C","F"]:
-    curr = input("Currency, *F*iat or *C*rypto?")
+    a = my_select("Unit for specifying buy?", ["Fiat", "Crypto"])
+    curr = a[0]
 
-amout = input("Amount to buy? ")
+amout = my_input("Amount to buy?")
 amout = float(amout)
 
 buyprice = prices[1]
@@ -42,17 +78,18 @@ elif curr == "F":
     amout = amout / buyprice
     amout = round(amout, 6)
 
-reply = input(f"Buying {amout} from {pair}, okay? ")
+reply = my_confirm(f"Buying {amout} from {pair}, okay?")
 
-if reply != "y":
+if not reply:
+    print("Aborting")
     sys.exit()
 
 result = kraken.createMarketBuyOrder(pair, amout)
 print(result)
 
-reply = input("Create 1-2-3-4 % limit sell orders? ")
-
-if reply != "y":
+reply = my_confirm("Create 1-2-3-4 % limit sell orders?")
+if not reply:
+    print("Aborting")
     sys.exit()
 
 for incr in range(1,5):
